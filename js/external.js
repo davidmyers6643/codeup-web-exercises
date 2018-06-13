4d115c03899f06b433f6f9815fbe9c8465c6f7a1
David Myers <davidmyers@Davids-MacBook-Air.local>
2018-06-13 09:57:01 -0500

    js_solutions

--- js/external.js
+++ js/external.js
@@ -5,25 +5,33 @@ alert('Welcome to my Website');
 var userInput = prompt('What is your favorite color?');
 alert('WOW, my favorite color is ' + userInput + ' as well.');
 
-var little_mermaid_days_rented = lm_days_rented;
 
-var brother_bear_days_rented = bb_days_rented;
+var rentalPerDayDollars = prompt("what is the daily rental rate?");
 
-var hercules_days_rented = h_days_rented;
 
-var price_of_daily_rental = 3.00;
+var littleMermaidDays = prompt('How many days did you  want to rent Little Mermaid?');
+console.log(littleMermaidDays);
 
-var lm_days_rented = prompt('How many days did you  want to rent Little Mermaid?');
-console.log(lm_days_rented);
+var brotherBearDays = prompt('How many days did you  want to rent Bother Bear?');
+console.log(brotherBearDays);
 
-var bb_days_rented = prompt('How many days did you  want to rent Bother Bear?');
-console.log(bb_days_rented);
+var herculesDays = prompt('How many days did you  want to rent Hercules?');
+console.log(herculesDays);
 
-var h_days_rented = prompt('How many days did you  want to rent Hercules?');
-console.log(h_days_rented);
+var totalRentalCost =
+    (parseInt(littleMermaidDays)
+        + parseInt(brotherBearDays)
+        + parseInt(herculesDays))
+    * parseInt(rentalPerDayDollars)
+console.log(totalRentalCost);
 
-console.log(bb_days_rented * price_of_daily_rental)
-+= (h_days_rented * price_of_daily_rental)
-+= (lm_days_rented * price_of_daily_rental);
+var totalRentalDays = parseInt(littleMermaidDays)
+    + parseInt(brotherBearDays)
+    + parseInt(herculesDays)
+
+alert('Your total cost for ' + (totalRentalDays) + ' days of rental is $'  +  (totalRentalCost));
+
+
+
 
 

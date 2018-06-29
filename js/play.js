2423a8814b35d5d502779a194ccdcfc4e007332f
David Myers <davidmyers@Davids-MacBook-Air.local>
2018-06-29 15:52:42 -0500

    jq_p.m

--- js/play.js
+++ js/play.js
@@ -17,15 +17,16 @@ $('li').css('font-size', '20px').css('background-color', 'aqua');
 
 $('#code').css('font-Family', 'cursive').css('font-size', '25px').css('background-color', 'lightblue');
 
-$('#title').click(function () {
-    $(this).css('font-size', '130px').css('background-color', 'yellow');
+// $('#title').click(function () {
+//     $(this).css('font-size', '130px').css('background-color', 'yellow');
+
 });
 
 $('li').dblclick(function () {
     $(this).css('background', 'yellow');
 });
     $('p').dblclick(function () {
-        $('p').css('background-color', 'salmon').css('font-size', '18px');
+        $('p').css('background-color', '#c69653').css('font-size', '18px');
     });
 $('li').hover(function () {
     $(this).css('color', 'red');
@@ -33,9 +34,50 @@ $('li').hover(function () {
   $(this).css('color', 'black');
 });
 
+$('#playInput').keyup(function(event) {
+    console.log(event.key);
+    console.log(event.keyCode);
+    if(event.key === 'd') {
+        alert('That is the 1st letter in my name');
+    }
+});
+
+$('#playButton').click(function () {
+    $('#playInput').off();
+});
 alert($('h1').text());
+//
+// var kkeys = [], konami = "38,38,40,40,37,39,37,39,66,65";
+//
+// $(document).keydown(function(e) {
+//
+//     kkeys.push( e.keyCode );
+//
+//     if ( kkeys.toString().indexOf( konami ) >= 0 ) {
+//
+//         $(document).unbind('keydown',arguments.callee);
+//
+//         // do something awesome
+//         $("body").addClass("konami");
+//
+//     }
+
+});
+
 
 // window.onload = function() {
     // alert( 'The page has finished loading!' );
+
 // };
-});
+// });
+
+// $('#title').click(function () {
+//     var currentColor = $(this).css('background-color');
+//     if (currentColor == 'rgb(0, 255, 255') {
+//         $(this).css('background-color', 'white');
+//     } else {
+//         $(this).css('background-color', 'cyan');
+//     }
+// });
+
+

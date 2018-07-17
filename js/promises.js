71670c584b61207b36eb046f353cc5037105b05b
David Myers <davidmyers@Davids-MacBook-Air.local>
2018-07-17 00:57:51 -0500

    midnightPush

--- js/promises.js
+++ js/promises.js
@@ -1,16 +1,56 @@
-const wait = (number) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (number === 1000) {
-                resolve();
-            } else if (number === 3000) {
-                resolve();
-            }
-        }, number);
-    });
-};
-//
-// // wait.then(() => console.log('resolved!'));
-// // wait.catch(() => console.log('rejected!'));
-wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+// const wait = (number) => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (number === 1000) {
+//                 resolve();
+//             } else if (number === 3000) {
+//                 resolve();
+//             }
+//         }, number);
+//     });
+// };
+// wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+// wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+
+
+
+// cleaner approach.
+
+const wait = num => {return new Promise((res, rej) =>
+            {setTimeout( res, num);
+        });
+    };
+
+wait(4000).then(() => console.log('You\'ll see this after 1 second after the 3 second log'));
 wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+wait(5000).then(() => document.write('DONKEYs'));
+// wait(5000).then(() => document.getElementById("demo").innerHTML = 'donkeys');
+
+
+
+
+const getLastEvent = (username) => {
+    const token = '480422bc7f13cd0cb048be4e93bd8f8fee6a395a';
+    const url = `https://api.github.com/users/${username}/events/public`;
+    return fetch(url, {headers: {'Authorization': `token ${token}`}})
+        .then(response => response.json())
+};
+
+// this returns date of last PushEvent.
+getLastEvent('davidmyers6643')
+    .then(data => data.filter(bob => bob.type ==='PushEvent'))
+    .then(data => console.log(data[0].created_at));
+
+// this returns date of last event.
+    getLastEvent('davidmyers6643').then((data) => {
+        console.log(data[0].created_at);
+});
+
+// same return because last event was a PushEvent.
+
+
+
+
+// token// 480422bc7f13cd0cb048be4e93bd8f8fee6a395a
+
+// GET /users/:username/events

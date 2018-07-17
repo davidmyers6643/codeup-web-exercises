e9d563d40c1fd9d0613d08889c7caadf164c0998
David Myers <davidmyers@Davids-MacBook-Air.local>
2018-07-17 12:15:50 -0500

    promises_am_push

--- js/promises.js
+++ js/promises.js
@@ -21,10 +21,11 @@ const wait = num => {return new Promise((res, rej) =>
         });
     };
 
-wait(4000).then(() => console.log('You\'ll see this after 1 second after the 3 second log'));
+wait(4000).then(() => console.log('You\'ll see this 1 second after the 3 second log'));
 wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
-wait(5000).then(() => document.write('DONKEYs'));
-// wait(5000).then(() => document.getElementById("demo").innerHTML = 'donkeys');
+// wait(5000).then(() => document.write('DONKEYs'));
+wait(5000).then(() => document.getElementById("demo").innerHTML = 'Donkeys!');
+wait(5000).then(() => console.log('Donkeys Rule!'));
 
 
 
@@ -33,22 +34,47 @@ const getLastEvent = (username) => {
     const token = '480422bc7f13cd0cb048be4e93bd8f8fee6a395a';
     const url = `https://api.github.com/users/${username}/events/public`;
     return fetch(url, {headers: {'Authorization': `token ${token}`}})
-        .then(response => response.json())
-};
 
+};
 // this returns date of last PushEvent.
 getLastEvent('davidmyers6643')
+    .then(response => response.json())
     .then(data => data.filter(bob => bob.type ==='PushEvent'))
     .then(data => console.log(data[0].created_at));
 
-// this returns date of last event.
-    getLastEvent('davidmyers6643').then((data) => {
-        console.log(data[0].created_at);
-});
+// // this returns date of last event.
+    getLastEvent('davidmyers6643')
+        .then(response => response.json())
+        .then(data => console.log(data[0].created_at));
 
-// same return because last event was a PushEvent.
+// // same return because last event was a PushEvent.
+
+// --------------------------second way---------------------------
 
 
+
+const getLastEvent2 = (username) => {
+    const token = '480422bc7f13cd0cb048be4e93bd8f8fee6a395a';
+    const url = `https://api.github.com/users/${username}/events/public`;
+    return fetch(url, {headers: {'Authorization': `token ${token}`}})
+        .then(response => response.json())
+};
+
+
+// this returns date of last PushEvent.
+getLastEvent2('davidmyers6643')
+
+    .then(data => data.filter(events => events.type ==='PushEvent'))
+    .then(data => console.log(data[0].created_at));
+
+
+// // this returns date of last event.
+getLastEvent2('davidmyers6643')
+
+    .then(data => console.log(data[0].created_at));
+// // same return because last event was a PushEvent.
+
+// ----------------------------------info---------------------------------//
 
 
 // token// 480422bc7f13cd0cb048be4e93bd8f8fee6a395a
